e86e0908650292f7a422206fac4811d49d90e43b
KyleK <kylekatarnls@gmail.com>
2019-06-16 15:38:50 +0200

    Add background colors

--- src/SimpleCli/SimpleCli.php
+++ src/SimpleCli/SimpleCli.php
@@ -8,6 +8,8 @@ use SimpleCli\Command\Version;
 abstract class SimpleCli
 {
     protected $colors = [
+        'black' => '0;30',
+        'dark_gray' => '1;30',
         'blue' => '0;34',
         'light_blue' => '1;34',
         'green' => '0;32',
@@ -18,24 +20,54 @@ abstract class SimpleCli
         'light_red' => '1;31',
         'purple' => '0;35',
         'light_purple' => '1;35',
+        'brown' => '0;33',
         'yellow' => '1;33',
+        'light_gray' => '0;37',
+        'white' => '1;37',
+    ];
+
+    protected $backgrounds = [
+        'black' => '40',
+        'red' => '41',
+        'green' => '42',
+        'yellow' => '43',
+        'blue' => '44',
+        'magenta' => '45',
+        'cyan' => '46',
+        'light_gray' => '47',
     ];
 
     protected $lastText = '';
+
+    protected $escapeCharacter = "\033";
 
     protected $currentCompletion = null;
 
     abstract public function getCommands(): array;
 
-    public function __construct(array $colors = null)
+    public function __construct(array $colors = null, array $backgrounds = null)
     {
         if ($colors) {
             $this->colors = $colors;
         }
 
+        if ($backgrounds) {
+            $this->backgrounds = $backgrounds;
+        }
+
         if (extension_loaded('readline') && function_exists('readline_completion_function')) {
             readline_completion_function([$this, 'autocomplete']);
         }
+    }
+
+    /**
+     * Set a custom string for escape command in CLI strings.
+     *
+     * @param string $escapeCharacter
+     */
+    public function setEscapeCharacter(string $escapeCharacter): void
+    {
+        $this->escapeCharacter = $escapeCharacter;
     }
 
     public function autocomplete(string $start = '')
@@ -51,9 +83,24 @@ abstract class SimpleCli
         return $this->currentCompletion ? ($this->currentCompletion)($start) : [];
     }
 
-    protected function colorize(string $text = '', string $color = null)
+    protected function getColorCode(string $color, array $colors = null)
     {
-        return "\033[".$color.'m'.$text."\033[0m";
+        $colors = $colors ?: $this->colors;
+        $color = $colors[$color] ?? $color;
+
+        return$this->escapeCharacter.'['.$color.'m';
+    }
+
+    protected function colorize(string $text = '', string $color = null, string $background = null)
+    {
+        if (!$color && !$background) {
+            return $text;
+        }
+
+        $color = $color ? $this->getColorCode($color) : '';
+        $background = $background ? $this->getColorCode($background, $this->backgrounds) : '';
+
+        return $color.$background.$text.$this->escapeCharacter.'[0m';
     }
 
     public function read($prompt, $completion = null)
@@ -69,7 +116,7 @@ abstract class SimpleCli
             $length = strlen($this->lastText);
         }
 
-        echo "\033[".$length.'D';
+        echo $this->escapeCharacter.'['.$length.'D';
     }
 
     public function write(string $text = '', string $color = null): void
--- tests/SimpleCli/SimpleCliTest.php
+++ tests/SimpleCli/SimpleCliTest.php
@@ -2,13 +2,35 @@
 
 namespace Tests\SimpleCli;
 
-use Tests\SimpleCli\DemoCommand\DemoCommand;
+use Tests\SimpleCli\DemoApp\DemoCli;
 
+/**
+ * @coversDefaultClass \SimpleCli\SimpleCli
+ */
 class SimpleCliTest extends TestCase
 {
+    /**
+     * @covers ::write
+     */
     public function testWrite()
     {
-        $command = new DemoCommand();
+        $command = new DemoCli();
+
+        static::assertOutput('Hello world', function () use ($command) {
+            $command->write('Hello world');
+        });
+
+        static::assertOutput('[ESCAPE][0;31mHello world[ESCAPE][0m', function () use ($command) {
+            $command->write('Hello world', 'red');
+        });
+    }
+
+    /**
+     * @covers ::writeLine
+     */
+    public function testWriteLine()
+    {
+        $command = new DemoCli();
 
         static::assertOutput('Hello world', function () use ($command) {
             $command->write('Hello world');
@@ -16,6 +38,10 @@ class SimpleCliTest extends TestCase
 
         static::assertOutput("Hello world\n", function () use ($command) {
             $command->writeLine('Hello world');
+        });
+
+        static::assertOutput("[ESCAPE][0;31mHello world\n[ESCAPE][0m", function () use ($command) {
+            $command->writeLine('Hello world', 'red');
         });
     }
 }
