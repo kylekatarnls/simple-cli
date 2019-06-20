1add1112329635052d8b0180a61230648bfc12d6
KyleKatarn <kylekatarnls@gmail.com>
2019-06-20 17:18:39 +0200

    Create Input/Output standalone traits

--- src/SimpleCli/SimpleCli.php
+++ src/SimpleCli/SimpleCli.php
@@ -8,9 +8,13 @@ use ReflectionObject;
 use SimpleCli\Command\Usage;
 use SimpleCli\Command\Version;
 use SimpleCli\Composer\InstalledPackage;
+use SimpleCli\Traits\Input;
+use SimpleCli\Traits\Output;
 
 abstract class SimpleCli
 {
+    use Input, Output;
+
     /**
      * @var string|null
      */
@@ -51,70 +55,10 @@ abstract class SimpleCli
      */
     protected $options;
 
-    /**
-     * @var array
-     */
-    protected $colors = [
-        'black'        => '0;30',
-        'dark_gray'    => '1;30',
-        'blue'         => '0;34',
-        'light_blue'   => '1;34',
-        'green'        => '0;32',
-        'light_green'  => '1;32',
-        'cyan'         => '0;36',
-        'light_cyan'   => '1;36',
-        'red'          => '0;31',
-        'light_red'    => '1;31',
-        'purple'       => '0;35',
-        'light_purple' => '1;35',
-        'brown'        => '0;33',
-        'yellow'       => '1;33',
-        'light_gray'   => '0;37',
-        'white'        => '1;37',
-    ];
-
-    /**
-     * @var array
-     */
-    protected $backgrounds = [
-        'black'      => '40',
-        'red'        => '41',
-        'green'      => '42',
-        'yellow'     => '43',
-        'blue'       => '44',
-        'magenta'    => '45',
-        'cyan'       => '46',
-        'light_gray' => '47',
-    ];
-
-    /**
-     * @var string
-     */
-    protected $lastText = '';
-
-    /**
-     * @var string
-     */
-    protected $escapeCharacter = "\033";
-
-    /**
-     * @var \Closure|callable|array|null
-     */
-    protected $currentCompletion = null;
-
     public function __construct(array $colors = null, array $backgrounds = null)
     {
-        if ($colors) {
-            $this->colors = $colors;
-        }
-
-        if ($backgrounds) {
-            $this->backgrounds = $backgrounds;
-        }
-
-        if (extension_loaded('readline') && function_exists('readline_completion_function')) {
-            readline_completion_function([$this, 'autocomplete']);
-        }
+        $this->setColors($colors, $backgrounds);
+        $this->recordAutocomplete();
     }
 
     /**
@@ -267,149 +211,6 @@ abstract class SimpleCli
     }
 
     /**
-     * Set a custom string for escape command in CLI strings.
-     *
-     * @param string $escapeCharacter
-     */
-    public function setEscapeCharacter(string $escapeCharacter): void
-    {
-        $this->escapeCharacter = $escapeCharacter;
-    }
-
-    /**
-     * Get possible completions for a given start.
-     *
-     * @param string $start
-     *
-     * @return string[]
-     */
-    public function autocomplete(string $start = ''): array
-    {
-        if (is_array($this->currentCompletion)) {
-            $length = strlen($start);
-
-            return array_filter($this->currentCompletion, function ($suggestion) use ($length, $start) {
-                return substr($suggestion, 0, $length) === $start;
-            });
-        }
-
-        return $this->currentCompletion ? ($this->currentCompletion)($start) : [];
-    }
-
-    protected function getColorCode(string $color, array $colors = null): string
-    {
-        $colors = $colors ?: $this->colors;
-        $color = $colors[$color] ?? $color;
-
-        return $this->escapeCharacter.'['.$color.'m';
-    }
-
-    /**
-     * Return $text with given color and background color.
-     *
-     * @param string      $text
-     * @param string|null $color
-     * @param string|null $background
-     *
-     * @return string
-     */
-    public function colorize(string $text = '', string $color = null, string $background = null): string
-    {
-        if (!$color && !$background) {
-            return $text;
-        }
-
-        $color = $color ? $this->getColorCode($color) : '';
-        $background = $background ? $this->getColorCode($background, $this->backgrounds) : '';
-
-        return $color.$background.$text.$this->escapeCharacter.'[0m';
-    }
-
-    /**
-     * Ask the user $prompt and return the CLI input.
-     *
-     * @param string              $prompt
-     * @param array|callable|null $completion
-     *
-     * @return string
-     */
-    public function read($prompt, $completion = null): string
-    {
-        $this->currentCompletion = $completion;
-
-        return readline($prompt);
-    }
-
-    /**
-     * Rewind CLI cursor $length characters behind, if $length is omitted, use the last written string length.
-     *
-     * @param int|null $length
-     */
-    public function rewind(int $length = null): void
-    {
-        if ($length === null) {
-            $length = strlen($this->lastText);
-        }
-
-        echo $this->escapeCharacter.'['.$length.'D';
-    }
-
-    /**
-     * Output $text with given color and background color.
-     *
-     * @param string      $text
-     * @param string|null $color
-     * @param string|null $background
-     */
-    public function write(string $text = '', string $color = null, string $background = null): void
-    {
-        $this->lastText = $text;
-
-        if ($color) {
-            $text = $this->colorize($text, $color, $background);
-        }
-
-        echo $text;
-    }
-
-    /**
-     * Output $text with given color and background color and add a new line.
-     *
-     * @param string      $text
-     * @param string|null $color
-     * @param string|null $background
-     */
-    public function writeLine(string $text = '', string $color = null, string $background = null): void
-    {
-        $this->write("$text\n", $color, $background);
-    }
-
-    /**
-     * Replace last written line with $text with given color and background color.
-     *
-     * @param string      $text
-     * @param string|null $color
-     * @param string|null $background
-     */
-    public function rewrite(string $text = '', string $color = null, string $background = null): void
-    {
-        $this->rewind();
-        $this->write($text, $color, $background);
-    }
-
-    /**
-     * Replace last written line with $text with given color and background color and re-add the new line.
-     *
-     * @param string      $text
-     * @param string|null $color
-     * @param string|null $background
-     */
-    public function rewriteLine(string $text = '', string $color = null, string $background = null): void
-    {
-        $this->write("\r$text", $color, $background);
-    }
-
-    /**
      * Get the list of commands included those provided by SimpleCli.
      *
      * @return array
@@ -469,11 +270,11 @@ abstract class SimpleCli
         $length = strlen($code) + 1;
         $result = null;
 
-        $source = preg_replace_callback('/^'.preg_quote($code).'( ([^\n]*(\n+'.str_repeat(' ', $length).'[^\n]*)*))?/m', function ($match) use (&$result, $length) {
-            $result = str_replace("\n".str_repeat(' ', $length), "\n", $match[2] ?? '');
+        $source = strval(preg_replace_callback('/^'.preg_quote($code).'( ([^\n]*(\n+'.str_repeat(' ', $length).'[^\n]*)*))?/m', function ($match) use (&$result, $length) {
+            $result = (string) str_replace("\n".str_repeat(' ', $length), "\n", $match[2] ?? '');
 
             return '';
-        }, $source);
+        }, $source) ?: '');
 
         $source = trim($source, "\n");
 
@@ -489,7 +290,7 @@ abstract class SimpleCli
      */
     public function getOptionDefinition(string $name): array
     {
-        foreach ($this->expectedOptions as $property => $definition) {
+        foreach ($this->expectedOptions as $definition) {
             if (in_array($name, $definition['names'])) {
                 return $definition;
             }
@@ -512,7 +313,7 @@ abstract class SimpleCli
             $argument = $this->extractAnnotation($doc, 'argument') !== null;
             $option = $this->extractAnnotation($doc, 'option');
             $values = $this->extractAnnotation($doc, 'values');
-            $var = str_replace('boolean', 'bool', $this->extractAnnotation($doc, 'var'));
+            $var = str_replace('boolean', 'bool', $this->extractAnnotation($doc, 'var') ?: 'string');
 
             if ($option === '') {
                 $option = "$name, ".substr($name, 0, 1);
@@ -629,13 +430,16 @@ abstract class SimpleCli
     }
 
     /**
+     * Cast argument/option according to type in the definition.
+     *
      * @param string $parameter
      * @param array  $optionDefinition
+     *
      * @return string
      */
     public function getParameterValue(string $parameter, array $optionDefinition)
     {
-        if (!settype($parameter, $optionDefinition['type'])) {
+        if (!settype($parameter, $optionDefinition['type'] ?? 'string')) {
             throw new InvalidArgumentException(
                 "Cannot cast $parameter to ".$optionDefinition['type']
             );
