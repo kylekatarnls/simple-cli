cf40ee5ec47db38c05bb4b5b1a1fd8fce567b595
KyleKatarn <kylekatarnls@gmail.com>
2019-09-17 15:03:19 +0200

    Add abstract CommandBase and EnhancedCommandBase classes

---
+++ src/SimpleCli/EnhancedCommandBase.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleCli;
+
+use SimpleCli\Options\Quiet;
+use SimpleCli\Options\Verbose;
+
+abstract class EnhancedCommandBase extends CommandBase
+{
+    use Quiet, Verbose;
+}
